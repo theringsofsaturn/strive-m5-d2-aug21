75fbb1fa806bdde9307588336c82173d807f743d
Riccardo Gulin <riccardo@nucleode.com>
2021-10-19 10:44:52 +0200

    Adds Express and initial config

---
+++ src/server.js
@@ -0,0 +1,10 @@
+// OLD IMPORT SYNTAX const express = require("express")
+import express from "express" // NEW IMPORT SYNTAX (remember to add type: "module" in package.json to use new syntax)
+
+const server = express()
+
+const port = 3002
+
+server.listen(port, () => {
+  console.log("Server running on port: ", port)
+})
